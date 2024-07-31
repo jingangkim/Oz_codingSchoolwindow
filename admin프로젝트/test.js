da796d2a3062c5b67187b80ba4a2c849e4da7741
jingangkim <seeu4534@gmail.com>
2024-07-31 15:49:45 +0900

    main

--- admin프로젝트/test.js
+++
@@ -1,9 +0,0 @@
-function isis() {
-    var button = document.getElementById("myButton");
-  
-    if (button.innerText === "Hello") {
-      button.innerText = "World";
-    } else {
-      button.innerText = "Hello";
-    }
-  }
